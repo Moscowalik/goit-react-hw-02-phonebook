fbf4a4427d3682cd7843bf662df1c37187f512e7
Maxim Moskalik <moscowalik95@gmail.com>
2022-05-07 21:41:02 +0300

    add new files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import ReactDOM from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './base.css';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
